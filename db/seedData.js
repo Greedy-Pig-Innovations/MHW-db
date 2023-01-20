e03a22f8cf88257fc0ebd69db60391187169c0db
Nlsn <n911luna@gmail.com>
2023-01-20 12:55:48 -0600

    starting tables

--- db/seedData.js
+++ db/seedData.js
@@ -5,6 +5,20 @@ const { client } = require("./client");
 const dropTables = async () => {
     try {
       console.log("Starting to drop tables...");
+
+      await client.query(`
+      DROP TABLE IF EXISTS ailments;
+      DROP TABLE IF EXISTS armor;
+      DROP TABLE IF EXISTS sets;
+      DROP TABLE IF EXISTS charms;
+      DROP TABLE IF EXISTS decorations;
+      DROP TABLE IF EXISTS events;
+      DROP TABLE IF EXISTS items;
+      DROP TABLE IF EXISTS locations;
+      DROP TABLE IF EXISTS monsters;
+      DROP TABLE IF exists skills;
+      DROP TABLE IF EXISTS weapons;
+      `);
   
       console.log("Finished dropping tables!");
     } catch (error) {
@@ -16,6 +30,73 @@ const dropTables = async () => {
   const createTables = async () => {
     try {
       console.log("Starting to build tables...");
+
+      await client.query(`
+      CREATE TABLE ailments(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        description TEXT NOT NULL,
+        recovery JSON NOT NULL,
+        protection JSON NOT NULL
+      );
+      CREATE TABLE armor(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        type VARCHAR(255) NOT NULL,
+        rank VARCHAR(255) NOT NULL,
+        rarity INTEGER NOT NULL,
+        defence JSON NOT NULL,
+        resistances JSON NOT NULL,
+        slots JSON[] NOT NULL,
+        skills JSON[] NOT NULL,
+        set JSON NOT NULL,
+        assets JSON NOT NULL,
+        crafting JSON[] NOT NULL,
+        gender VARCHAR(255) NOT NULL
+      );
+      CREATE TABLE sets (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        rank VARCHAR(255) NOT NULL,
+        pieces JSON[] NOT NULL,
+        bonus JSON NOT NULL
+      );
+      CREATE TABLE charms(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        ranks JSON[] NOT NULL
+      );
+      CREATE TABLE decorations(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL
+      );
+      CREATE TABLE events(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL
+      );
+      CREATE TABLE items(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL
+      );
+      CREATE TABLE locations(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL
+      );
+      CREATE TABLE monsters(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL
+      );
+      CREATE TABLE skills(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL
+      );
+      CREATE TABLE weapons(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL
+      ); 
+
+      `);
+
   
       console.log("Finished building tables!");
     } catch (error) {
@@ -27,6 +108,8 @@ const dropTables = async () => {
   const rebuildDB = async () => {
     try {
       client.connect();
+      await dropTables();
+      await createTables();
   
     } catch (error) {
       console.error("Error rebuilding database!");
