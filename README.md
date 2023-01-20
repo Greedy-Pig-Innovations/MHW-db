c8cf4cb6bfa43e4ac3224377256b524284ca66c6
Nlsn <n911luna@gmail.com>
2023-01-20 10:53:12 -0600

    first commit

--- README.md
+++
@@ -1,4 +0,0 @@
-# Backend-Temp
-
-A basic back-end template relying on postgresql, node.js and express.js.
-In the db folder, client.js file, change ${NAME} to the desired database, otherwise localhost:8080 is default.
--- api/index.js
+++ api/index.js
@@ -1,37 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const jwt = require('jsonwebtoken');
-const { JWT_SECRET } = process.env;
-
-router.use(async (req, res, next) => {
-    const prefix = 'Bearer ';
-    const auth = req.header('Authorization');
-  
-    if (!auth) { // nothing to see here
-      next();
-    } else if (auth.startsWith(prefix)) {
-      const token = auth.slice(prefix.length);
-      console.log("tokeN", JWT_SECRET);
-      try {
-        const {id} = jwt.verify(token, JWT_SECRET);
-        console.log("id", id);
-        if (id) {
-          req.user = await getUserById(id);
-          next();
-        }
-      } catch ({ name, message }) {
-        next({ name, message });
-      }
-    } else {
-      next({
-        name: 'AuthorizationHeaderError',
-        message: `Authorization token must start with ${ prefix }`
-      });
-    }
-  });
-
-
 
 
   router.use((error,req,res,next) => {
